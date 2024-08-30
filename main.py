678a6dd7010776e5af3f4282b0c23396ff0be3f5
Magnus Andersson <mpja69@gmail.com>
2024-08-30 07:46:43 +0200

    player can move

--- main.py
+++ main.py
@@ -12,7 +12,6 @@ def main():
     clock = pygame.time.Clock()
     dt = 0
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
-
     player  = Player(SCREEN_WIDTH/2, SCREEN_HEIGHT/2)
 
     while True:
@@ -22,12 +21,14 @@ def main():
                 return
 
         # Update
+        player.update(dt)
+
         # Draw
         screen.fill(0)
         player.draw(screen)
-
-
         pygame.display.flip()
+
+        # FPS 60
         dt = clock.tick(60) / 1000
 
 
--- player.py
+++ player.py
@@ -1,6 +1,7 @@
 import pygame
+from pygame.transform import rotate
 from circleshape import CircleShape
-from constants import PLAYER_RADIUS
+from constants import PLAYER_RADIUS, PLAYER_SPEED, PLAYER_TURN_SPEED
 
 
 class Player(CircleShape):
@@ -9,7 +10,7 @@ class Player(CircleShape):
         self.position = pygame.Vector2(x,y)
         self.rotation = 0
 
-    # in the player class
+    # The points in the triangle
     def triangle(self):
         forward = pygame.Vector2(0, 1).rotate(self.rotation)
         right = pygame.Vector2(0, 1).rotate(self.rotation + 90) * self.radius / 1.5
@@ -18,5 +19,30 @@ class Player(CircleShape):
         c = self.position - forward * self.radius + right
         return [a, b, c]
 
+
+    def move(self, dt):
+        v = pygame.Vector2(0,1)
+        v = v.rotate(self.rotation)
+        v *= PLAYER_SPEED * dt
+        self.position += v
+
+
+    def rotate(self, dt):
+        self.rotation += PLAYER_TURN_SPEED * dt
+
+
+    def update(self, dt):
+        keys = pygame.key.get_pressed()
+        if keys[pygame.K_a]:
+            self.rotate(-dt)
+        if keys[pygame.K_d]:
+            self.rotate(dt)
+        if keys[pygame.K_w]:
+            self.move(dt)
+        if keys[pygame.K_s]:
+            self.move(-dt)
+
+
     def draw(self, screen):
         pygame.draw.polygon(screen, "white", self.triangle(), 2)
+
