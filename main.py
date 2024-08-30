c637072bafa612a04ac584c785d026781b4eb00e
Magnus Andersson <mpja69@gmail.com>
2024-08-30 08:43:16 +0200

    added sprite groups, and the 'new way' of adding the player to the groups, (containers)

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 import pygame
+from asteroid import Asteroid
 from constants import *
 from player import Player
 
@@ -9,11 +10,19 @@ def main():
     print("Screen height:", SCREEN_HEIGHT)
 
     pygame.init()
-    clock = pygame.time.Clock()
-    dt = 0
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+    clock = pygame.time.Clock()
+    
+    updatable = pygame.sprite.Group()
+    drawable = pygame.sprite.Group()
+    asteroids = pygame.sprite.Group()
+
+    Player.containers = (updatable, drawable)
     player  = Player(SCREEN_WIDTH/2, SCREEN_HEIGHT/2)
 
+    Asteroid.containers = (asteroids, updatable, drawable)
+
+    dt = 0
     while True:
         # User Input
         for event in pygame.event.get():
@@ -21,15 +30,18 @@ def main():
                 return
 
         # Update
-        player.update(dt)
+        updatable.update(dt)
 
         # Draw
         screen.fill(0)
-        player.draw(screen)
+        for item in drawable:
+            item.draw(screen)
+
         pygame.display.flip()
 
         # FPS 60
         dt = clock.tick(60) / 1000
+
 
 
 if __name__ == "__main__":
